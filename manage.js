986441111c8013e3a92c64f360206c060ca19ab1
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-04 21:26:17 +0200

    card js component prepared

--- manage.js
+++ manage.js
@@ -19,3 +19,23 @@ for (let counter = 1; counter <= numberOfQuestions; counter++) {
   });
 }
 
+
+
+
+const card = document.createElement("article");
+card.classList.add("quiz__card");
+card.innerHTML = `
+          <h2 class="quiz__card__question">What is the meaning of life</h2>
+          <button data-js="showAnswer3" class="showAnswer">Show answer</button>
+          <h2 class="answer" hidden data-js="answer3">42</h2>
+          <article class="quiz__card__tags">
+            <button class="quiz__card__tags__tag">Personal</button>
+            <button class="quiz__card__tags__tag">Smalltalk</button>
+            <button class="quiz__card__tags__tag">Jakob</button>
+          </article>
+          <button data-js="bookmark3" class="card__bookmarktoggle">
+            Bookmark
+          </button>
+`;
+const cardContainer = document.querySelector('[data-js=cardContainer]')
+cardContainer.appendChild(card)
