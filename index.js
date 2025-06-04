f43adc73e57f2c725c13a802622499480392107e
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-04 19:08:12 +0200

    show answer is working -- toggling

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-console.clear()
-console.log('⭐️ Welcome to J Town ⭐️');
+console.clear();
+console.log("⭐️ Welcome to J Town ⭐️");
+const answers = [];
+const showAnswers = [];
+let numberOfQuestions = 3;
+
+for(let counter = 1; counter <= numberOfQuestions; counter++){
+  answers.push(document.querySelector(`[data-js=answer${counter}]`));
+  showAnswers.push(document.querySelector(`[data-js=showAnswer${counter}]`));
+  // console.log(showAnswers[counter-1]);
+  showAnswers[counter - 1].addEventListener("click", () => {
+    answers[counter - 1].toggleAttribute("hidden");
+  });
+
+}
+// answers[0].removeAttribute("hidden");
+console.log(answers);
+console.log(showAnswers);
