8f0cbab5af312aa63eb592bbf847b7b0a1287bdd
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-04 19:28:55 +0200

    bookmarks toggle

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ console.log("⭐️ Welcome to J Town ⭐️");
 let numberOfQuestions = 3;
 const answers = [];
 const showAnswers = [];
-const bookmark = document.querySelector('[data-js=bookmark]')
-console.log(bookmark);
+const bookmarks = [];
 
-
-for(let counter = 1; counter <= numberOfQuestions; counter++){
+for (let counter = 1; counter <= numberOfQuestions; counter++) {
   answers.push(document.querySelector(`[data-js=answer${counter}]`));
   showAnswers.push(document.querySelector(`[data-js=showAnswer${counter}]`));
   showAnswers[counter - 1].addEventListener("click", () => {
     answers[counter - 1].toggleAttribute("hidden");
   });
 
+  bookmarks.push(document.querySelector(`[data-js=bookmark${counter}]`));
+  const currentBookmark = bookmarks[counter - 1];
+  currentBookmark.addEventListener("click", () => {
+    currentBookmark.classList.toggle('card__bookmarktoggle--active')
+  });
 }
-
-
